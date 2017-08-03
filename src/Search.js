fe0da993406d2fef7dfce7c6b1fc8f36cce00616
btahir <bilal@Bilals-iMac.local>
2017-08-02 23:01:52 -0400

    updated search for Book component
    updated search for Book component

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
+import Book from './Book';
 
 class Search extends React.Component {
   state = {
@@ -61,25 +62,10 @@ class Search extends React.Component {
               <ol className="books-grid">
                 {matches.map(match => (
                   <li className="search-book-result" key={match.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${match.imageLinks.thumbnail})` }} />
-                        <div className="book-shelf-changer">
-                          <select
-                            value={match.shelf}
-                            onChange={event => onAddShelf(match, event.target.value)}
-                          >
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{match.title}</div>
-                      <div className="book-authors">{match.authors}</div>
-                    </div>
+                    <Book
+                      selectedBook={match}
+                      updateShelf={onAddShelf}
+                    />
                   </li>
                 ))}
               </ol>
