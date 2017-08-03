800de5db734df7a9d96d1b3051089a37fb62260a
btahir <bilal@Bilals-iMac.local>
2017-08-02 21:48:03 -0400

    Eslinted!
    fixed up code (indentations/semicolons) based on eslint package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import './App.css'
-import { Route, Link} from 'react-router-dom'
-import Search from './Search'
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+import Search from './Search';
 
 class BooksApp extends React.Component {
   state = {
-    allBooks: []
+    allBooks: [],
   }
 
   // retrieve all books on our shelves
   componentDidMount() {
     BooksAPI.getAll().then((allBooks) => {
-      this.setState({allBooks});
+      this.setState({ allBooks });
     });
   }
 
   // update book shelf
   updateShelf = (book, newshelf) => {
-    this.setState( (p) => {
-      let bookUpdate = p.allBooks.filter( ourBook => ourBook.id === book.id);
+    this.setState((p) => {
+      const bookUpdate = p.allBooks.filter(ourBook => ourBook.id === book.id);
       bookUpdate[0].shelf = newshelf;
     });
     BooksAPI.update(book, newshelf);
@@ -27,7 +27,7 @@ class BooksApp extends React.Component {
 
   // Add new books from search
   addShelf = (book, newshelf) => {
-    this.setState( (p) => {
+    this.setState((p) => {
       book.shelf = newshelf;
       p.allBooks.push(book);
     });
@@ -38,13 +38,19 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={ () => (
-          <Search
-            ourBooks={this.state.allBooks}
-            onAddShelf={this.addShelf}
-          />
-        )} />
-        <Route exact path="/" render={ () => (
+        <Route
+          path="/search"
+          render={() => (
+            <Search
+              ourBooks={this.state.allBooks}
+              onAddShelf={this.addShelf}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          render={() => (
             <div className="list-books">
               <div className="list-books-title">
                 <h1>MyReads</h1>
@@ -55,14 +61,18 @@ class BooksApp extends React.Component {
                     <h2 className="bookshelf-title">Currently Reading</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
-                        {this.state.allBooks.map( book => (
+                        {this.state.allBooks.map(book => (
                           book.shelf === 'currentlyReading' && (
                             <li key={book.id}>
                               <div className="book">
                                 <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
                                   <div className="book-shelf-changer">
-                                    <select value={book.shelf} onChange={ (event) => this.updateShelf( book, event.target.value )}>
+                                    <select
+                                      value={book.shelf}
+                                      onChange={event =>
+                                        this.updateShelf(book, event.target.value)}
+                                    >
                                       <option value="none" disabled>Move to...</option>
                                       <option value="currentlyReading">Currently Reading</option>
                                       <option value="wantToRead">Want to Read</option>
@@ -84,14 +94,18 @@ class BooksApp extends React.Component {
                     <h2 className="bookshelf-title">Want to Read</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
-                        {this.state.allBooks.map( book => (
+                        {this.state.allBooks.map(book => (
                           book.shelf === 'wantToRead' && (
                             <li key={book.id}>
                               <div className="book">
                                 <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
                                   <div className="book-shelf-changer">
-                                    <select value={book.shelf} onChange={ (event) => this.updateShelf( book, event.target.value )}>
+                                    <select
+                                      value={book.shelf}
+                                      onChange={event =>
+                                        this.updateShelf(book, event.target.value)}
+                                    >
                                       <option value="none" disabled>Move to...</option>
                                       <option value="currentlyReading">Currently Reading</option>
                                       <option value="wantToRead">Want to Read</option>
@@ -113,14 +127,17 @@ class BooksApp extends React.Component {
                     <h2 className="bookshelf-title">Read</h2>
                     <div className="bookshelf-books">
                       <ol className="books-grid">
-                        {this.state.allBooks.map( book => (
+                        {this.state.allBooks.map(book => (
                           book.shelf === 'read' && (
                             <li key={book.id}>
                               <div className="book">
                                 <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
                                   <div className="book-shelf-changer">
-                                    <select value={book.shelf} onChange={ (event) => this.updateShelf( book, event.target.value )}>
+                                    <select
+                                      value={book.shelf}
+                                      onChange={event => this.updateShelf(book, event.target.value)}
+                                    >
                                       <option value="none" disabled>Move to...</option>
                                       <option value="currentlyReading">Currently Reading</option>
                                       <option value="wantToRead">Want to Read</option>
@@ -144,10 +161,11 @@ class BooksApp extends React.Component {
                 <Link to="/search">Add a book</Link>
               </div>
             </div>
-        )} />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/Search.js
+++ src/Search.js
@@ -1,92 +1,95 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 
 class Search extends React.Component {
+  state = {
+    matches: [],
+    query: '',
+    myBooks: [],
+  }
 
-	state = {
-		matches: [],
-		query: '',
-		myBooks: []
-	}
+  // search function and setting state for query and matches
+  searchQuery = (query) => {
+    query = query.trim();
+    const searchResults = BooksAPI.search(query, 20);
+    searchResults.then(function(response) {
+      if (typeof response !== 'undefined' && typeof response.length !== 'undefined') {
+      // syncing shelf of search results to our bookshelf
+        response.map((items) => {
+          this.props.ourBooks.map((book) => {
+            if (book.id === items.id) {
+              items.shelf = book.shelf;
+            }
+          });
+        });
+        // set state when we get search result
+        this.setState((state) => {
+          state.matches = response;
+          state.query = query;
+        });
+      } else {
+        // set state when we do not get search result
+        this.setState((state) => {
+          state.matches = [];
+          state.query = query;
+        });
+      }
+    }.bind(this));
+  }
 
-	// search function and setting state for query and matches
-	searchQuery = (query) => {
-		query = query.trim();
-		let searchResults = BooksAPI.search(query, 20);
-		searchResults.then(function(response) {
-			if(typeof response !== 'undefined' && typeof response.length !== 'undefined') {
-				// syncing shelf of search results to our bookshelf
-				response.map(items => {
-					this.props.ourBooks.map(book => {
-						if (book.id === items.id) {
-							items.shelf = book.shelf;
-						}});
-				});
-				// set state when we get search result
-				this.setState(state => {
-					state.matches = response;
-					state.query = query;
-				});
-			} else {
-				// set state when we do not get search result
-				this.setState(state => {
-					state.matches = [];
-					state.query = query;
-				});
-			}
-		}.bind(this));
-	}
+  render() {
+    const { onAddShelf } = this.props;
+    const { matches, query } = this.state;
 
-	render() {
-
-		const { onAddShelf } = this.props;
-		const { matches, query } = this.state;
-
-		return (
-			<div className='search-results'>
-	          <div className="search-books">
-	            <div className="search-books-bar">
-	              <Link className="close-search" to="/">Close</Link>
-	              <div className="search-books-input-wrapper">
-	                <input type="text"
-	                placeholder="Search by title or author"
-					value={query}
-					onChange={ (event) => this.searchQuery( event.target.value )}/>
-	              </div>
-	            </div>
-					{matches.length > 0 ? (
-					    <div className="search-books-results">
-							<ol className="books-grid">
-								{matches.map(match => (
-									<li className="search-book-result" key={match.id}>
-										<div className="book">
-											<div className="book-top">
-											  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${match.imageLinks.thumbnail})` }}></div>
-												  <div className="book-shelf-changer">
-												    <select value={match.shelf} onChange={ (event) => onAddShelf( match, event.target.value )}>
-												      <option value="none" disabled>Move to...</option>
-												      <option value="currentlyReading">Currently Reading</option>
-												      <option value="wantToRead">Want to Read</option>
-												      <option value="read">Read</option>
-												      <option value="none">None</option>
-												    </select>
-												  </div>
-												</div>
-												<div className="book-title">{match.title}</div>
-											<div className="book-authors">{match.authors}</div>
-										</div>
-									</li>
-								))}
-							</ol>
-					    </div>
-					) : (<div className="search-books-results">No Search Results</div>)}
-				</div>
-			</div>
-			)
-	}
-
+    return (
+      <div className='search-results'>
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to="/">Close</Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={event => this.searchQuery(event.target.value)}
+              />
+            </div>
+          </div>
+          {matches.length > 0 ? (
+            <div className="search-books-results">
+              <ol className="books-grid">
+                {matches.map(match => (
+                  <li className="search-book-result" key={match.id}>
+                    <div className="book">
+                      <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${match.imageLinks.thumbnail})` }} />
+                        <div className="book-shelf-changer">
+                          <select
+                            value={match.shelf}
+                            onChange={event => onAddShelf(match, event.target.value)}
+                          >
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{match.title}</div>
+                      <div className="book-authors">{match.authors}</div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          ) : (<div className="search-books-results">No Search Results</div>)}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
 
