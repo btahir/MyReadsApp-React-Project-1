d7b07e7ae883fc04737df95aaa5bcf156dd207c3
btahir <bilal@Bilals-iMac.local>
2017-08-02 22:55:08 -0400

    created Book component
    created Book component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 import Search from './Search';
+import Book from './Book';
 
 class BooksApp extends React.Component {
   state = {
@@ -64,26 +65,10 @@ class BooksApp extends React.Component {
                         {this.state.allBooks.map(book => (
                           book.shelf === 'currentlyReading' && (
                             <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                                  <div className="book-shelf-changer">
-                                    <select
-                                      value={book.shelf}
-                                      onChange={event =>
-                                        this.updateShelf(book, event.target.value)}
-                                    >
-                                      <option value="none" disabled>Move to...</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
+                              <Book
+                                selectedBook={book}
+                                updateShelf={this.updateShelf}
+                              />
                             </li>
                           )
                         ))}
@@ -97,26 +82,10 @@ class BooksApp extends React.Component {
                         {this.state.allBooks.map(book => (
                           book.shelf === 'wantToRead' && (
                             <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                                  <div className="book-shelf-changer">
-                                    <select
-                                      value={book.shelf}
-                                      onChange={event =>
-                                        this.updateShelf(book, event.target.value)}
-                                    >
-                                      <option value="none" disabled>Move to...</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
+                              <Book
+                                selectedBook={book}
+                                updateShelf={this.updateShelf}
+                              />
                             </li>
                           )
                         ))}
@@ -130,25 +99,10 @@ class BooksApp extends React.Component {
                         {this.state.allBooks.map(book => (
                           book.shelf === 'read' && (
                             <li key={book.id}>
-                              <div className="book">
-                                <div className="book-top">
-                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }} />
-                                  <div className="book-shelf-changer">
-                                    <select
-                                      value={book.shelf}
-                                      onChange={event => this.updateShelf(book, event.target.value)}
-                                    >
-                                      <option value="none" disabled>Move to...</option>
-                                      <option value="currentlyReading">Currently Reading</option>
-                                      <option value="wantToRead">Want to Read</option>
-                                      <option value="read">Read</option>
-                                      <option value="none">None</option>
-                                    </select>
-                                  </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                              </div>
+                              <Book
+                                selectedBook={book}
+                                updateShelf={this.updateShelf}
+                              />
                             </li>
                           )
                         ))}
---
+++ src/Book.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Book = props => (
+  <div className="book">
+    <div className="book-top">
+      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.selectedBook.imageLinks.thumbnail})` }} />
+      <div className="book-shelf-changer">
+        <select
+          value={props.selectedBook.shelf}
+          onChange={event =>
+            props.updateShelf(props.selectedBook, event.target.value)}
+        >
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    </div>
+    <div className="book-title">{props.selectedBook.title}</div>
+    <div className="book-authors">{props.selectedBook.authors}</div>
+  </div>
+
+
+);
+
+export default Book;
